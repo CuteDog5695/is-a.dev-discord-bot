215cae48a81ca3c95213a2fe3048b2398f55cd57
andrewstech <andrew@win11react.com>
2023-05-28 10:28:46 +0100

    Update auth.js

--- components/auth.js
+++ components/auth.js
@@ -4,7 +4,7 @@ const GITHUB_ID = process.env.GITHUB_ID;
 const SERVER_URL = process.env.SERVER_URL;
 
 function getAccessToken(DiscordID) {
-    const authUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_ID}&scope=public_repo%20user:email&redirect_uri=https://${SERVER_URL}/auth/handler?discordId=${discordId}`;
+    const authUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_ID}&scope=public_repo%20user:email&redirect_uri=https://${SERVER_URL}/auth/handler?discordId=${DiscordID}`;
     return authUrl;
 }
 
