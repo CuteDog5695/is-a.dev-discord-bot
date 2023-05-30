6b895a84d013ac93efbafd8f1fd4692341b8f2ef
andrewstech <andrew@win11react.com>
2023-05-30 16:58:29 +0000

    register command

---
+++ commands/register.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("register")
+        .setDescription("Register an Is-A-Dev Subdomain!")
+        .addStringOption(option =>
+            option.setName("subdomain")
+                .setDescription("Enter the subdomain")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("record_type")
+                .setDescription("Select the record type")
+                .setRequired(true)
+                .addChoice("A", "A")
+                .addChoice("CNAME", "CNAME")
+                .addChoice("MX", "MX")
+                .addChoice("TXT", "TXT")
+        )
+        .addStringOption(option =>
+            option.setName("record_string")
+                .setDescription("Enter the record string")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const subdomain = interaction.options.getString("subdomain");
+        const recordType = interaction.options.getString("record_type");
+        const recordString = interaction.options.getString("record_string");
+
+        let regexPattern;
+
+        switch (recordType) {
+            case "A":
+                regexPattern = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+                break;
+            case "CNAME":
+                regexPattern = /^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/;
+                break;
+            case "MX":
+                regexPattern = /^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/;
+                break;
+            case "TXT":
+                regexPattern = /^.*$/;
+                break;
+            default:
+                await interaction.reply("Invalid record type.");
+                return;
+        }
+
+        if (!regexPattern.test(recordString)) {
+            await interaction.reply("Invalid record string.");
+            return;
+        }
+
+        await interaction.reply(`Subdomain: ${subdomain}\nRecord Type: ${recordType}\nRecord String: ${recordString}`);
+    },
+};
