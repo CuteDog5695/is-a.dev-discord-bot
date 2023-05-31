ab99cba5479469bd2bf893f084cb699d5897d2c8
William Harrison <william@williamdavidharrison.com.au>
2023-06-01 06:46:52 +0800

    Update commit.js

--- components/commit.js
+++ components/commit.js
@@ -1,12 +1,16 @@
 const User = require("../models/user");
 const { Octokit } = require("@octokit/rest");
+
 require("dotenv").config();
+
 const { SlashCommandBuilder, EmbedBuilder, Client } = require("discord.js");
+
 async function CommitChanges(id, subdomain, type, data, interaction) {
     const githubUser = await User.findOne({ userid: id });
     const token = githubUser.gittoken;
     const username = githubUser.githubid;
     const email = githubUser.email;
+
     if (process.env.DEBUG) {
         console.log("COMMIT FUNCTION.");
         console.log("id: " + id);
@@ -14,30 +18,35 @@ async function CommitChanges(id, subdomain, type, data, interaction) {
         console.log("username: " + username);
         console.log("email: " + email);
     }
-    const octokit = new Octokit({
-        auth: token,
-    });
+
+    const octokit = new Octokit({ auth: token });
+
     if (type === "A" || type === "MX") {
         data = JSON.stringify(data.split(",").map((s) => s.trim()));
     } else {
         data = `"${data.trim()}"`;
     }
+
     content = `{
-        "owner": {
-           "username": "${username}",
-           "email": "${email}"
-       },
-       "record": {
-           "${type}": ${data.toLowerCase()}
-       }
+    "owner": {
+       "username": "${username}",
+       "email": "${email}"
+   },
+   "record": {
+       "${type}": ${data.toLowerCase()}
    }
-   `;
+}
+`;
+
     const record = Buffer.from(content).toString("base64");
+
     const commit = await octokit.repos.createOrUpdateFileContents({
         owner: username,
         repo: "register",
         path: "domains/" + subdomain.toLowerCase() + ".is-a.dev.json",
-        message: `feat(domain): ${subdomain.toLowerCase().replace(/\.[^/.]+$/, "")}.is-a.dev`,
+        message: `feat(domain): ${subdomain
+            .toLowerCase()
+            .replace(/\.[^/.]+$/, "")}.is-a.dev`,
         content: record,
         committer: {
             name: username,
@@ -48,11 +57,23 @@ async function CommitChanges(id, subdomain, type, data, interaction) {
             email: email,
         },
     });
-    const embed = new EmbedBuilder().setTitle(`Registering ${subdomain}.is-a.dev`).addFields({ name: "Forked ", value: "✅", inline: true }, { name: "Commited ", value: "✅", inline: true }, { name: "PR Opened ", value: "❌", inline: true }).setColor("#00b0f4").setFooter({
-        text: "is-a.dev",
-        icon_url: "https://raw.githubusercontent.com/is-a-dev/register/main/media/logo.png",
-    });
+
+    const embed = new EmbedBuilder()
+        .setTitle(`Registering ${subdomain}.is-a.dev`)
+        .addFields(
+            { name: "Forked ", value: "✅", inline: true },
+            { name: "Commited ", value: "✅", inline: true },
+            { name: "PR Opened ", value: "❌", inline: true }
+        )
+        .setColor("#00b0f4")
+        .setFooter({
+            text: "is-a.dev",
+            icon_url:
+                "https://raw.githubusercontent.com/is-a-dev/register/main/media/logo.png",
+        });
+
     await interaction.editReply({ embeds: [embed] });
     return commit;
 }
+
 exports.CommitChanges = CommitChanges;
