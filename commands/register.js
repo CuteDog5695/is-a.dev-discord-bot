4be2d3eb9d41a371e574ea3da0014bc4cdc13f70
William Harrison <william@williamdavidharrison.com.au>
2023-06-01 06:38:23 +0800

    feat: prettier

--- commands/register.js
+++ commands/register.js
@@ -7,24 +7,29 @@ const User = require("../models/user.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("register")
-        .setDescription("Register an Is-A-Dev Subdomain!")
-        .addStringOption(option =>
-            option.setName("subdomain")
+        .setDescription("Register an is-a.dev Subdomain!")
+        .addStringOption((option) =>
+            option
+                .setName("subdomain")
                 .setDescription("Enter the subdomain")
                 .setRequired(true)
         )
-        .addStringOption(option =>
-            option.setName("record_type")
+        .addStringOption((option) =>
+            option
+                .setName("record_type")
                 .setDescription("Select the record type")
                 .setRequired(true)
                 .addChoices(
-                    { name: 'CNAME', value: 'CNAME' },
-                    { name: 'A', value: 'A' },
-                    { name: 'TXT', value: 'TXT' },
-                ))
-        .addStringOption(option =>
-            option.setName("record_string")
-                .setDescription("Enter the record string")
+                    { name: "A", value: "A" },
+                    { name: "CNAME", value: "CNAME" },
+                    { name: "MX", value: "MX" },
+                    { name: "TXT", value: "TXT" }
+                )
+        )
+        .addStringOption((option) =>
+            option
+                .setName("record_string")
+                .setDescription("Enter the record value(s)")
                 .setRequired(true)
         ),
     async execute(interaction) {
@@ -32,17 +37,12 @@ module.exports = {
         const recordType = interaction.options.getString("record_type");
         const recordString = interaction.options.getString("record_string");
 
-        if (!interaction.member.roles.cache.some((role) => role.name === "Bot Beta Tester")) {
-            await interaction.reply("Only beta testers can use this command!");
-            return;
-        }
+        if (!interaction.member.roles.cache.some((role) => role.name === "Bot Beta Tester"))
+            return await interaction.reply("Only beta testers can use this command!");
 
         const githubUser = await User.findOne({ userid: interaction.user.id });
 
-        if (!githubUser) {
-            await interaction.reply("You are not logged in!");
-            return;
-        }
+        if (!githubUser) return await interaction.reply("You are not logged in!");
 
         let regexPattern;
 
@@ -60,32 +60,33 @@ module.exports = {
                 regexPattern = /^.*$/;
                 break;
             default:
-                await interaction.reply("Invalid record type.");
-                return;
+                return await interaction.reply("Invalid record type.");
         }
 
-        if (!regexPattern.test(recordString)) {
-            await interaction.reply("Invalid record string.");
-            return;
-        }
-        // embed
+        if (!regexPattern.test(recordString))
+            return await interaction.reply("Invalid record string.");
+
+        // Embed
         const subdomain = subdomains.replace(/\.is-a\.dev$/, "");
+
         const embed = new EmbedBuilder()
-        .setTitle(`Registering ${subdomain}.is-a.dev`)
-        .addFields(
-            { name: 'Forked', value: '❌', inline: true },
-            { name: 'Commited', value: '❌', inline: true },
-            { name: 'PR Opened', value: '❌', inline: true },
-        )
-        .setColor('#00b0f4')
-        .setFooter({
-            text: 'is-a.dev',
-            icon_url: 'https://raw.githubusercontent.com/is-a-dev/register/main/media/logo.png'
-        });
+            .setTitle(`Registering ${subdomain}.is-a.dev`)
+            .addFields(
+                { name: "Forked", value: "❌", inline: true },
+                { name: "Commited", value: "❌", inline: true },
+                { name: "PR Opened", value: "❌", inline: true }
+            )
+            .setColor("#00b0f4")
+            .setFooter({
+                text: "is-a.dev",
+                icon_url: "https://raw.githubusercontent.com/is-a-dev/register/main/media/logo.png"
+            });
+
         await interaction.reply({ embeds: [embed] });
-        await fork(interaction.user.id, interaction, subdomain)
+        await fork(interaction.user.id, interaction, subdomain);
+
         // add a 3 second delay to allow the fork to complete
-        await new Promise(r => setTimeout(r, 3000));
+        await new Promise((r) => setTimeout(r, 3000));
         await CommitChanges(interaction.user.id, subdomain, recordType, recordString, interaction);
         await OpenPR(interaction.user.id, subdomain, interaction);
     },
