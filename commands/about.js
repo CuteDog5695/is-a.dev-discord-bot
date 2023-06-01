627360916ca272d35f4d6db3d5b01bcf1a7ee098
andrewstech <andrew@win11react.com>
2023-06-01 23:28:06 +0000

    alow bot commands to be used in dm's and other servers

--- commands/about.js
+++ commands/about.js
@@ -3,7 +3,6 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder().setName("about").setDescription("About the bot!"),
     async execute(interaction) {
-        
-        await interaction.reply("Pong!");
+        await interaction.reply("This bot is open source! Check it out at https://github.com/andrewstech/is-a-dev-discord");
     },
 };
