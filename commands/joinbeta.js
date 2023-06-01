201096af4f8dbd210757819b4b316b5fa86dc89e
andrewstech <andrew@win11react.com>
2023-06-01 22:45:15 +0000

    join beta

--- commands/joinbeta.js
+++ commands/joinbeta.js
@@ -15,8 +15,29 @@ module.exports = {
           body: [
             {
               type: "text",
-              text: "Welcome to the beta testing program!",
+              text: "Welcome to the beta testing program! By clicking the button below, you agree to the following terms and conditions:",
             },
+            {
+                type: "text",
+                text: "1. Only use in command channels.",
+            },
+            {
+                type: "text",
+                text: "2. You will report any bugs you encounter.",
+
+            },
+            {
+                type: "text",
+                text: "3. You will not abuse any of the bot's features.",
+            },
+            {
+                type: "text",
+                text: "4. You will not use the bot to break any of the server's rules.",
+            },
+            {
+                type: "text",
+                text: "5. You will not use the bot to break any of Discord's Terms of Service.",
+            }
             {
               type: "button",
               label: "Start Testing",
