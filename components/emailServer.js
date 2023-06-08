62ac870a16f44c3d1b69285492a01d6504a1039a
andrewstech <andrew@win11react.com>
2023-06-08 22:13:47 +0000

    creds

--- components/emailServer.js
+++ components/emailServer.js
@@ -1,21 +1,24 @@
-const email = require("../models/email");
+const Email = require("../models/email");
 // smtp server
 const SMTPServer = require("smtp-server").SMTPServer;
 
 const SMTPserver = new SMTPServer({
-    onAuth(auth, session, callback) {
+    async onAuth(auth, session, callback) {
       const { username, password } = auth;
-
+      try {
+        // Check if the provided credentials are valid
+        const user = await Email.findOne({ username, passkey: password }).exec();
   
-      // Check if the provided credentials are valid
-      email.findOne({ username, passkey: password }, (err, user) => {
-        if (err || !user) {
+        if (!user || domain !== user.domain) {
           callback(new Error("Invalid username or password"));
-          console.log("Invalid username or password")
+          console.log("SMTP: INVALID CREDENTIALS")
         } else {
           callback(null, { user });
         }
-      });
+      } catch (error) {
+        callback(error);
+      }
+
     },
 });
 
