5204099cbf0e900f49a0ef0e20f1c84346f90895
andrewstech <andrew@win11react.com>
2023-06-29 21:20:46 +0000

    sha

--- components/DeleteFile.js
+++ components/DeleteFile.js
@@ -6,11 +6,19 @@ async function DeleteFile(domain, interaction) {
     const githubUser = await User.findOne({ userid: id });
     const token = githubUser.gittoken;
     const octokit = new Octokit({ auth: token });
+    // get the file sha
+    const file = await octokit.repos.getContent({
+        owner: githubUser.githubid,
+        repo: "register",
+        path: `domains/${domain}.json`,
+    });
+    const sha = file.data.sha;
     const deletefile = await octokit.repos.deleteFile({
         owner: githubUser.githubid,
         repo: "register",
         path: `domains/${domain}.json`,
         message: `Delete ${domain}.is-a.dev`,
+        sha: sha,
     });
     console.log(deletefile);
     const embed = new EmbedBuilder().setTitle(`Deleting ${subdomain}.is-a.dev`).addFields({ name: "Forked", value: "✅", inline: true }, { name: "Commited", value: "✅", inline: true }, { name: "PR Opened", value: "❌", inline: true }).setDescription(`Please wait for a staff member to review your PR.`).setColor("#00b0f4").setFooter({
