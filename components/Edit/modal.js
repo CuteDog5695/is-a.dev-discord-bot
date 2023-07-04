a05bfaab384b383bb24b0aa7eec1d4453439b415
andrewstech <andrew@win11react.com>
2023-07-04 22:20:37 +0000

    record content

--- components/Edit/modal.js
+++ components/Edit/modal.js
@@ -2,14 +2,35 @@ const { SlashCommandBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, T
 async function EditModal(interaction) {
         const domains = interaction.values[0];
         const domain = domains.replace(/\.is-a\.dev$/, "");
+        const response = await fetch(`https://raw.githubusercontent.com/is-a-dev/register/main/domains/${domain}.json`, {
+                headers: {
+                    "User-Agent": "is-a-dev-bot",
+                },
+            });
+        const data = await response.json();
+        const records = [];
+        Object.keys(data.record).forEach((record) => {
+            if (record === "A" || record === "MX") {
+                data.record[record].forEach((r) => {
+                    records.push(`**${record}** ${r}`);
+                });
+
+                return;
+            }
+
+            if (record === "URL") return records.push(`**${record}** ${data.record[record]}`);
+
+            records.push(`**${record}** ${data.record[record]}`);
+        });
         const modal = new ModalBuilder().setCustomId("editmodal").setTitle(`Edit: ${domains}`);
         const recordContent = new TextInputBuilder()
-            .setCustomId("content")
+            .setCustomId(domain)
             .setLabel("What is the new content?")
             .setRequired(true)
+            .setValue(records)
             // Paragraph means multiple lines of text.
             .setStyle(TextInputStyle.Short);
-
+        records = []
         const secondActionRow = new ActionRowBuilder().addComponents(recordContent);
 
         // Add inputs to the modal
