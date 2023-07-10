bc6a0cd41650b393b0ac7df969dcc48b7a94dc6e
andrewstech <andrew@win11react.com>
2023-07-10 21:57:09 +0000

    JSON ARRAY

--- components/web/listDomains.js
+++ components/web/listDomains.js
@@ -7,7 +7,16 @@ async function ListDomains(username) {
 
                 for (let i = 0; i < data.length; i++) {
                     if (data[i].owner.username.toLowerCase() === username.toLowerCase()) {
-                        results.push(data[i].domain);
+                        // push the domain record to the results array but remove email and note from the record
+                        // convert data[i].record} to an array
+                        const record = data[i].record;
+                        const arr = Object.entries(record)[0];
+                    
+                        
+                        results.push({ "domain": data[i].domain, "record": arr});
+                        // add the domain to the results array
+                        //results.push(data[i].domain);
+                        // set found to true
                         found = true;
                     }
                 }
