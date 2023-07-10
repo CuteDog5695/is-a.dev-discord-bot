2fcda49c22465895129486d2797da7e03c0c01dc
andrewstech <andrew@win11react.com>
2023-07-10 22:13:40 +0000

    Array changes

--- components/web/listDomains.js
+++ components/web/listDomains.js
@@ -1,26 +1,37 @@
 async function ListDomains(username) {
     let results = [];
-    await fetch("https://raw-api.is-a.dev")
-            .then((response) => response.json())
-            .then(async (data) => {
-                
 
-                for (let i = 0; i < data.length; i++) {
-                    if (data[i].owner.username.toLowerCase() === username.toLowerCase()) {
-                        // push the domain record to the results array but remove email and note from the record
-                        // convert data[i].record} to an array
-                        const record = data[i].record;
-                        const arr = Object.entries(record)[0];
+    // Assuming 'username' is defined and assigned a value
+
+    // Assuming 'fetch' is a valid function that retrieves data from a given URL
+    await fetch("https://raw-api.is-a.dev")
+        .then((response) => response.json())
+        .then(async (data) => {
+            // Code inside the fetch promise handler
+            // It receives the response and parses it as JSON data
+
+            for (let i = 0; i < data.length; i++) {
+                // Iterates over each element in the data array
+                
+                if (data[i].owner.username.toLowerCase() === username.toLowerCase()) {
+                    // Checks if the username (case-insensitive) matches the current data record's owner
                     
-                        
-                        results.push({ "domain": data[i].domain, "record": arr});
-                        // add the domain to the results array
-                        //results.push(data[i].domain);
-                        // set found to true
-                        found = true;
-                    }
+                    const record = data[i].record;
+                    const arr = Object.entries(record).map(([type, value]) => ({ type, value }));
+                    // Extracts the record property from the current data record
+                    // Converts the record object to an array of objects with 'type' and 'value' keys
+                    
+                    results.push({ "domain": data[i].domain,  "record": arr });
+                    // Adds a new object to the results array
+                    // The object contains the domain and the converted record array
+                    
+                    found = true;
+                    // Sets a variable 'found' to true
                 }
-            });
+            }
+        });
+
     return results;
+
 }
 exports.ListDomains = ListDomains;
