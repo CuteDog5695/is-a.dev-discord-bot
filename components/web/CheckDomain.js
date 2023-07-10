6cf5e2cec074a1424f7fe878e6a139db2a4cfc88
andrewstech <andrew@win11react.com>
2023-07-10 20:52:38 +0000

    FIx domain

--- components/web/CheckDomain.js
+++ components/web/CheckDomain.js
@@ -1,22 +1,17 @@
-function CheckDomain(domain) {
+async function CheckDomain(domain) {
     // check if domain is available
-    let found = false;
-    fetch("https://raw-api.is-a.dev")
-        .then((response) => response.json())
-        .then((data) => {
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].domain === domain) {
-                    found = true;
-                }
-            }
-        }
-        );
-        if (found) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    const subdomain = domain.replace(/\.is-a\.dev$/, "");
+    const response = await fetch(`https://api.github.com/repos/is-a-dev/register/contents/domains/${subdomain}.json`, {
+        headers: {
+            "User-Agent": "is-a-dev-bot",
+        },
+    });
+    if (response.status === 404) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 exports.CheckDomain = CheckDomain;
