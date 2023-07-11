8b95280916740b65ae5f2baaa8ecce7f287655d6
andrewstech <andrew@win11react.com>
2023-07-11 14:57:22 +0000

    Try now

--- components/web/CheckDomain.js
+++ components/web/CheckDomain.js
@@ -3,17 +3,17 @@ async function CheckDomain(subdomain) {
     let response = null
      response = await fetch(`https://api.github.com/repos/is-a-dev/register/contents/domains/${subdomain}.json`, {
         headers: {
-            "User-Agent": "is-a-dev-bot",
+            "User-Agent": "is-a-dev",
         },
     });
     console.log(response.status);
     if (response.status === 200) {
-        const json = { "status": "unavalible" };
-        return json;
+        const jsonu = { "status": "unavalible" };
+        return jsonu;
     }
     else {
-        const json = { "status": "available" };
-        return json;
+        const jsonp = { "status": "available" };
+        return jsonp;
     }
 }
 
