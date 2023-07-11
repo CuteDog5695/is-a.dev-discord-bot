36e29abaf602b2810d98542ae24c5cfd992d7b1a
andrewstech <andrew@win11react.com>
2023-07-11 14:44:31 +0000

    Fix

--- components/web/CheckDomain.js
+++ components/web/CheckDomain.js
@@ -1,6 +1,9 @@
+const { listeners } = require("../../models/prdata");
+
 async function CheckDomain(subdomain) {
     // check if domain is available
-    const response = await fetch(`https://api.github.com/repos/is-a-dev/register/contents/domains/${subdomain}.json`, {
+    let response = null
+     response = await fetch(`https://api.github.com/repos/is-a-dev/register/contents/domains/${subdomain}.json`, {
         headers: {
             "User-Agent": "is-a-dev-bot",
         },
