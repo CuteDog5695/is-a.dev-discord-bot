8785bc61752a0b4bec1e4ff0d3211989b9000481
andrewstech <andrew@win11react.com>
2023-07-11 14:26:50 +0000

    Fix

--- components/web/CheckDomain.js
+++ components/web/CheckDomain.js
@@ -1,6 +1,5 @@
-async function CheckDomain(domain) {
+async function CheckDomain(subdomain) {
     // check if domain is available
-    const subdomain = domain.replace(/\.is-a\.dev$/, "");
     const response = await fetch(`https://api.github.com/repos/is-a-dev/register/contents/domains/${subdomain}.json`, {
         headers: {
             "User-Agent": "is-a-dev-bot",
