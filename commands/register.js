88ba96839bced4f9d2f9352821b11d3d10a64dda
andrewstech <andrew@win11react.com>
2023-12-19 15:47:35 +0000

    WTF DID THE LOGIN CHECK GO

--- commands/register.js
+++ commands/register.js
@@ -1,11 +1,20 @@
 const { SlashCommandBuilder } = require("discord.js");
 const regiserDomain = require("../events/buttons/registerDomain");
+const user = require("../models/user");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("register")
         .setDescription("Register a domain."),
     async execute(interaction) {
+        await interaction.deferReply({ ephemeral: 'true' });
+        const data = await user.findOne({ _id: interaction.user.id });
+        if (!data) {
+            const embed = new EmbedBuilder()
+                .setDescription("You are not logged in!")
+                .setColor("#0096ff");
+            return await interaction.editReply({ embeds: [embed] });
+        }
         await regiserDomain(interaction);
     },
 };
