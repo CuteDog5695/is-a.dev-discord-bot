2580422ca66aef82a20a25507cda682f2615f91c
andrewstech <andrew@win11react.com>
2024-01-01 21:03:01 +0000

    Refactor ban appeal message and fix function call

--- events/buttons/AproveApeal.js
+++ events/buttons/AproveApeal.js
@@ -8,18 +8,15 @@ module.exports = async function (interaction) {
     console.log(id)
     id = id.split('-')[1]
     console.log(id)
-    let user = interaction.guild.members.cache.get(id)
-    // get username
-    let username = user.username
-    console.log(username)
+    
     // unban user
     await interaction.guild.members.unban(id)
     // send dm
     const embed = new EmbedBuilder()
         .setTitle('Ban Appeal')
-        .setDescription(`Dear ${username}, Thank you for your ban appeal. After further discussion, and consideration from the is-a-dev team, we are unbanning you from the server. You are advised to reflect on the original reasoning for your ban, and are advised to avoid doing the same thing once admitted back into the server.`)
+        .setDescription(`Dear user, Thank you for your ban appeal. After further discussion, and consideration from the is-a-dev team, we are unbanning you from the server. You are advised to reflect on the original reasoning for your ban, and are advised to avoid doing the same thing once admitted back into the server.`)
         .setColor('#00FF00')
-    await DM(user, embed)
+    await DM(interaction.client, id, embed)
 
     // reply to interaction
     await interaction.editReply({
