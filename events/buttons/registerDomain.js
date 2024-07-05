7bae806a70299abceda7ca6d1a5e7a0513033368
Mr Sticky Piston <97804691+MrStickyPiston@users.noreply.github.com>
2024-07-05 16:53:07 +0200

    bump Max domain length to 60, min to 2

--- events/buttons/registerDomain.js
+++ events/buttons/registerDomain.js
@@ -6,8 +6,8 @@ module.exports = async function (interaction) {
     const domainCheck = new TextInputBuilder()
         .setCustomId('DomainCheck')
         .setPlaceholder('Enter a subdomain')
-        .setMinLength(3)
-        .setMaxLength(17)
+        .setMinLength(2)
+        .setMaxLength(60)
         .setRequired(true)
         .setLabel('Enter the subdomain you want to register')
         .setStyle("Short");
