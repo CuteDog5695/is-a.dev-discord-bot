d511e0770d75db3d318e70d1d986e372d67039c7
CuteDog5695 <cutedog5695@gmail.com>
2024-07-29 09:52:42 +0300

    Update check.js

--- commands/check.js
+++ commands/check.js
@@ -18,7 +18,13 @@ module.exports = {
             .toLowerCase();
         // get the guild id from the interaction
         
-  
+if (subdomain.length < 1 || subdomain.length > 64) {
+            const sadEmbed = new EmbedBuilder()
+                .setDescription("The subdomain length must be between 1 and 64 characters.")
+                .setColor("#0096ff");
+            return await interaction.reply({ embeds: [sadEmbed] });    
+}
+        
         try {
             const response = await fetch(
                 `https://api.github.com/repos/is-a-dev/register/contents/domains/${subdomain}.json`,
